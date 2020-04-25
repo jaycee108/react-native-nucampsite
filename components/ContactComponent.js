2f9a635f7ffab2a4b408ea006a1b1d411a8174e5
Jennifer Casal <jennifercasal@gmail.com>
2020-04-25 12:07:06 -0700

    React Native Week 1 Workshop

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -13,7 +13,7 @@ class Contact extends Component {
         return(
             <ScrollView>
                  <Card 
-                    title='ContactInformation' 
+                    title='Contact Information' 
                     wrapperStyle={{margin: 20}}>
                     <Text>1 Nucamp Way</Text>
                     <Text>Seattle, WA 98001</Text>
